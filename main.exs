9cdeaf4f918eb0226801935b3659f089d415f9b7
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-11-09 16:08:32 +0100

    minimal reproduction

--- main.exs
+++ main.exs
@@ -11,6 +11,8 @@ Mix.install([
   {:phoenix, "~> 1.7"},
   # please test your issue using the latest version of LV from GitHub!
   {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", branch: "main", override: true},
+  {:phoenix_ecto, "~> 4.5"},
+  {:ecto, "~> 3.10"},
 ])
 
 # build the LiveView JavaScript assets (this needs mix and npm available in your path!)
@@ -23,19 +25,60 @@ defmodule Example.ErrorView do
   def render(template, _), do: Phoenix.Controller.status_message_from_template(template)
 end
 
+
+
+defmodule ExampleForm do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field :field_one, :string
+    embeds_one :nested_form, NestedExampleForm
+  end
+
+  def changeset(company_form, attrs \\ %{}) do
+    company_form
+    |> cast(attrs, [:field_one])
+    |> cast_embed(:nested_form)
+    |> validate_required([:field_one])
+    |> validate_length(:field_one, is: 2000)
+  end
+end
+
+defmodule NestedExampleForm do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :field_two, :string
+  end
+
+  def changeset(nested_form, attrs \\ %{}) do
+    nested_form
+    |> cast(attrs, [:field_two])
+    |> validate_required([:field_two])
+    |> validate_length(:field_two, is: 2000)
+  end
+end
+
 defmodule Example.HomeLive do
   use Phoenix.LiveView, layout: {__MODULE__, :live}
 
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :count, 0)}
+    {:ok,
+     socket
+     |> assign(form: %ExampleForm{} |> ExampleForm.changeset() |> to_form())}
   end
+
 
   def render("live.html", assigns) do
     ~H"""
     <script src="/assets/phoenix/phoenix.js"></script>
     <script src="/assets/phoenix_live_view/phoenix_live_view.js"></script>
     <%!-- uncomment to use enable tailwind --%>
-    <%!-- <script src="https://cdn.tailwindcss.com"></script> --%>
+    <script src="https://cdn.tailwindcss.com"></script>
     <script>
       let liveSocket = new window.LiveView.LiveSocket("/live", window.Phoenix.Socket)
       liveSocket.connect()
@@ -47,22 +90,183 @@ defmodule Example.HomeLive do
     """
   end
 
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <%= @count %>
-    <button phx-click="inc">+</button>
-    <button phx-click="dec">-</button>
+    <.form :let={f} for={@form} as={:form} phx-submit="validate" class="flex flex-col gap-4 max-w-sm mx-auto">
+      <.input field={f[:field_one]} label="Errors are shown" />
+      <.inputs_for :let={nested_form} field={f[:nested_form]}>
+        <.input field={nested_form[:field_two]} label="Errors are not shown" />
+      </.inputs_for>
+      <.button>Validate</.button>
+    </.form>
     """
   end
 
-  def handle_event("inc", _params, socket) do
-    {:noreply, assign(socket, :count, socket.assigns.count + 1)}
+  @impl true
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
   end
 
-  def handle_event("dec", _params, socket) do
-    {:noreply, assign(socket, :count, socket.assigns.count - 1)}
+  @impl true
+  def handle_event("validate", %{"form" => form}, socket) do
+    new_form =
+      %ExampleForm{}
+      |> ExampleForm.changeset(form)
+      |> to_form(action: :validate)
+
+    {:noreply,
+      socket
+      |> assign(:form, new_form)
+    }
   end
+
+
+
+  @doc """
+  Renders a button.
+
+  ## Examples
+
+      <.button>Send!</.button>
+      <.button phx-click="go" class="ml-2">Send!</.button>
+  """
+  attr :type, :string, default: nil
+  attr :class, :string, default: nil
+  attr :rest, :global, include: ~w(disabled form name value)
+
+  slot :inner_block, required: true
+
+  def button(assigns) do
+    ~H"""
+    <button
+      type={@type}
+      class={[
+        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
+        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        @class
+      ]}
+      {@rest}
+    >
+      <%= render_slot(@inner_block) %>
+    </button>
+    """
+  end
+
+  @doc """
+  Renders an input with label and error messages.
+
+  A `Phoenix.HTML.FormField` may be passed as argument,
+  which is used to retrieve the input name, id, and values.
+  Otherwise all attributes may be passed explicitly.
+
+  ## Types
+
+  This function accepts all HTML input types, considering that:
+
+    * You may also set `type="select"` to render a `<select>` tag
+
+    * `type="checkbox"` is used exclusively to render boolean values
+
+    * For live file uploads, see `Phoenix.Component.live_file_input/1`
+
+  See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+  for more information. Unsupported types, such as hidden and radio,
+  are best written directly in your templates.
+
+  ## Examples
+
+      <.input field={@form[:email]} type="email" />
+      <.input name="my-input" errors={["oh no!"]} />
+  """
+  attr :id, :any, default: nil
+  attr :name, :any
+  attr :label, :string, default: nil
+  attr :value, :any
+
+  attr :type, :string,
+    default: "text",
+    values: ~w(checkbox color date datetime-local email file month number password
+               range search select tel text textarea time url week)
+
+  attr :field, Phoenix.HTML.FormField,
+    doc: "a form field struct retrieved from the form, for example: @form[:email]"
+
+  attr :errors, :list, default: []
+  attr :checked, :boolean, doc: "the checked flag for checkbox inputs"
+  attr :prompt, :string, default: nil, doc: "the prompt for select inputs"
+  attr :options, :list, doc: "the options to pass to Phoenix.HTML.Form.options_for_select/2"
+  attr :multiple, :boolean, default: false, doc: "the multiple flag for select inputs"
+
+  attr :rest, :global,
+    include: ~w(accept autocomplete capture cols disabled form list max maxlength min minlength
+                multiple pattern placeholder readonly required rows size step)
+
+  def input(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
+    errors = if Phoenix.Component.used_input?(field), do: field.errors, else: []
+
+    assigns
+    |> assign(field: nil, id: assigns.id || field.id)
+    |> assign(:errors, Enum.map(errors, &inspect(&1, limit: :infinity, pretty: true)))
+    |> assign_new(:name, fn -> if assigns.multiple, do: field.name <> "[]", else: field.name end)
+    |> assign_new(:value, fn -> field.value end)
+    |> input()
+  end
+
+  # All other inputs text, datetime-local, url, password, etc. are handled here...
+  def input(assigns) do
+    ~H"""
+    <div>
+      <.label for={@id}><%= @label %></.label>
+      <input
+        type={@type}
+        name={@name}
+        id={@id}
+        value={Phoenix.HTML.Form.normalize_value(@type, @value)}
+        class={[
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 border-2 px-2 py-1",
+          @errors == [] && "border-zinc-300 focus:border-zinc-400",
+          @errors != [] && "border-rose-400 focus:border-rose-400"
+        ]}
+        {@rest}
+      />
+      <.error :for={msg <- @errors}><%= msg %></.error>
+    </div>
+    """
+  end
+
+  @doc """
+  Renders a label.
+  """
+  attr :for, :string, default: nil
+  slot :inner_block, required: true
+
+  def label(assigns) do
+    ~H"""
+    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+      <%= render_slot(@inner_block) %>
+    </label>
+    """
+  end
+
+  @doc """
+  Generates a generic error message.
+  """
+  slot :inner_block, required: true
+
+  def error(assigns) do
+    ~H"""
+    <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600">
+      <%= render_slot(@inner_block) %>
+    </p>
+    """
+  end
+
+
 end
+
+
 
 defmodule Example.Router do
   use Phoenix.Router
